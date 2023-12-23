a52f1247f3527dfa5ac2c138807ef6f412e4f874
Felipe Tampier <felipetampier@gmail.com>
2023-12-23 20:41:04 +0200

    Basic algo skips empty rows

--- render_sim/algorithms/alg_basic.py
+++ render_sim/algorithms/alg_basic.py
@@ -14,7 +14,9 @@ class AlgBasic(BaseRenderer):
         ), f"Incompatible N! {led_matrix.index.size} != {self.N}"
 
         for ix, pow2 in enumerate(self.get_2powerN(self.N)):
+            col_val = self.get_series_value(led_matrix.loc[ix, :])
             
-            self.add_frame_step(pow2, self.get_series_value(led_matrix.loc[ix, :]))
+            if col_val != 0:
+                self.add_frame_step(pow2, col_val)
 
         return self.steps
